0643fead1d8e4f3cb1245dac58fd3045c248c184
simhlakuva <simahlakuva@gmail.com>
2020-11-10 16:41:26 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BankingAppSH
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+       /* public void Information()
+        {
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+        }*/
+        
+        protected void Deposit_Clicked()
+        {
+           /* var account = myNewCustomer.ApplyForBankAccount();
+            account.DepositMoney(1500, DateTime.Now, "Stipend");*/
+            decimal amount;
+            string description;
+        }
+
+        protected  void WithdrawMoneyButton_Clicked()
+        {
+            //var account = myNewCustomer.ApplyForBankAccount();
+            //account.DepositMoney(1500, DateTime.Now, "Stipend");
+            decimal amount;
+            string description;
+        }
+
+        protected  void TransactionHistoryButton_Clicked()
+        {
+            //var account = myNewCustomer.ApplyForBankAccount();
+           // var history = account.GetTransactionHistory();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BankingAppSH
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+         
+        }
+    }
+
+}
